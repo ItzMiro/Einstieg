9724ddad959e401fb6157c0e943cba3e1affc5fe
ItzMiro <mathysmiro@gmail.com>
2024-12-23 22:48:53 +0100

    Initial Commit

---
+++ Einstieg/EinstiegApp.swift
@@ -0,0 +1,17 @@
+//
+//  EinstiegApp.swift
+//  Einstieg
+//
+//  Created by Miro Mathys on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EinstiegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EinstiegTests/EinstiegTests.swift
@@ -0,0 +1,17 @@
+//
+//  EinstiegTests.swift
+//  EinstiegTests
+//
+//  Created by Miro Mathys on 23.12.2024.
+//
+
+import Testing
+@testable import Einstieg
+
+struct EinstiegTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
